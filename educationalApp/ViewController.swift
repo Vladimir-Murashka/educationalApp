d492367b42bf204547ce20163d6afe3d27c33bb5
Vladimir Murashka <Murashka.Vladimir@yandex.ru>
2022-06-28 19:28:11 +0300

    Initial Commit

---
+++ educationalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  educationalApp
+//
+//  Created by Swift Learning on 28.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
